5aaef043a3e8fe5be851a0686614be98d1a597da
BaoBang <baobangb5@gmail.com>
2018-04-14 21:24:50 +0700

    test responese a string

--- src/main/java/com/baobang/piospa/controller/UserController.java
+++ src/main/java/com/baobang/piospa/controller/UserController.java
@@ -26,82 +26,99 @@ public class UserController {
 	@Autowired
 	private UserRepository mUserRepository;
 
-	/**
-	 * 
-	 * @return the list of users
-	 * 
-	 */
-	@RequestMapping(//
-			method = RequestMethod.GET, //
-			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-	public DataResult<List<User>> getAll() {
+	@RequestMapping(value = "/",
+			method = RequestMethod.GET)
+	public String getAll() {
 
-		List<User> users = mUserRepository.findAll();
 
-		return new DataResult<List<User>>(HttpCode.OK.getCode(), MessageResponese.SUCCESSED, users);
+		return "Hello Spring Boot and Heruko";
 	}
 
-	/**
-	 * The method get user profile by ID from parameter
-	 * 
-	 * @param usedId
-	 *            the ID of user
-	 * @return the user by userId
-	 * 
-	 */
-	@RequestMapping(//
-			value = "/{userId}", //
-			method = RequestMethod.GET, //
-			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-	public DataResult<User> getUserById(@PathVariable(value = "userId") int userId) {
-		DataResult<User> result = new DataResult<>();
-		Optional<User> option = mUserRepository.findById(userId);
-		User user = null;
-		if (option == null) {
-			result.setMessage(MessageResponese.USER_NOT_FOUND);
-			result.setStatusCode(HttpCode.NO_CONTENT.getCode());
-		} else {
-			user = option.get();
-			result.setMessage(MessageResponese.SUCCESSED);
-			result.setStatusCode(HttpCode.OK.getCode());
-		}
-		result.setData(user);
-		return result;
-	}
+	//
+	// /**
+	// *
+	// * @return the list of users
+	// *
+	// */
+	// @RequestMapping(//
+	// method = RequestMethod.GET, //
+	// produces = { MediaType.APPLICATION_JSON_VALUE,
+	// MediaType.APPLICATION_XML_VALUE })
+	// public DataResult<List<User>> getAll() {
+	//
+	// List<User> users = mUserRepository.findAll();
+	//
+	// return new DataResult<List<User>>(HttpCode.OK.getCode(),
+	// MessageResponese.SUCCESSED, users);
+	// }
+	//
+	// /**
+	// * The method get user profile by ID from parameter
+	// *
+	// * @param usedId
+	// * the ID of user
+	// * @return the user by userId
+	// *
+	// */
+	// @RequestMapping(//
+	// value = "/{userId}", //
+	// method = RequestMethod.GET, //
+	// produces = { MediaType.APPLICATION_JSON_VALUE,
+	// MediaType.APPLICATION_XML_VALUE })
+	// public DataResult<User> getUserById(@PathVariable(value = "userId") int
+	// userId) {
+	// DataResult<User> result = new DataResult<>();
+	// Optional<User> option = mUserRepository.findById(userId);
+	// User user = null;
+	// if (option == null) {
+	// result.setMessage(MessageResponese.USER_NOT_FOUND);
+	// result.setStatusCode(HttpCode.NO_CONTENT.getCode());
+	// } else {
+	// user = option.get();
+	// result.setMessage(MessageResponese.SUCCESSED);
+	// result.setStatusCode(HttpCode.OK.getCode());
+	// }
+	// result.setData(user);
+	// return result;
+	// }
 
 	/**
 	 * 
 	 * @param usedId
-	 * 			the id of user
+	 *            the id of user
 	 * @param user
-	 * 			user's info need to update
+	 *            user's info need to update
 	 * @return the user was updated
 	 *
 	 */
-//	@RequestMapping(//
-//			value = "/{userId}", //
-//			method = RequestMethod.PUT, //
-//			produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-//	public DataResult<User> updateUser(@PathVariable(value = "userId") int userId, @RequestBody User user) {
-//		DataResult<User> result;
-//		Optional<User> option = mUserRepository.findById(userId);
-//		if (option == null) {
-//			result = new DataResult<>(HttpCode.NOT_FOUND.getCode(), MessageResponese.USER_NOT_FOUND, null);
-//		} else {
-//			
-//			User newUser = option.get();
-//			
-//			newUser.setName(user.getName());
-//			newUser.setEmail(user.getEmail());
-//			newUser.setIsActive(user.getIsActive());
-//			newUser.setPassword(user.getPassword());
-//			newUser.setRememberToken(user.getRememberToken());
-//			newUser.setUpdatedAt(user.getUpdatedAt());
-//			newUser.setCreatedAt(user.getCreatedAt());
-//			
-//			result = new DataResult<>(HttpCode.OK.getCode(), MessageResponese.SUCCESSED, newUser);
-//		}
-//		return result;
-//	}
+	// @RequestMapping(//
+	// value = "/{userId}", //
+	// method = RequestMethod.PUT, //
+	// produces = { MediaType.APPLICATION_JSON_VALUE,
+	// MediaType.APPLICATION_XML_VALUE })
+	// public DataResult<User> updateUser(@PathVariable(value = "userId") int
+	// userId, @RequestBody User user) {
+	// DataResult<User> result;
+	// Optional<User> option = mUserRepository.findById(userId);
+	// if (option == null) {
+	// result = new DataResult<>(HttpCode.NOT_FOUND.getCode(),
+	// MessageResponese.USER_NOT_FOUND, null);
+	// } else {
+	//
+	// User newUser = option.get();
+	//
+	// newUser.setName(user.getName());
+	// newUser.setEmail(user.getEmail());
+	// newUser.setIsActive(user.getIsActive());
+	// newUser.setPassword(user.getPassword());
+	// newUser.setRememberToken(user.getRememberToken());
+	// newUser.setUpdatedAt(user.getUpdatedAt());
+	// newUser.setCreatedAt(user.getCreatedAt());
+	//
+	// result = new DataResult<>(HttpCode.OK.getCode(), MessageResponese.SUCCESSED,
+	// newUser);
+	// }
+	// return result;
+	// }
 
 }
